750e0b87528e81c84c5950eab24d2e806b0aeee4
切了特空 <lby3523@gmail.com>
2018-10-26 10:57:51 +0800

    ignore vscode

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sangreal-calendar',
-    version='0.0.11',
+    version='0.0.12',
     description=('trade_dt handle for A-share market'),
     install_requires=[
         'bs4',
